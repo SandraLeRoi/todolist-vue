a52038b7c7dcda8d6c876aa28cc1221c21cb1356
SandraLeRoi <oawx72@gmail.com>
2020-12-07 15:52:42 +0100

    travail du 07/12/2020

---
+++ src/components/TodolistComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+
+    <input type="text" v-model="newTask">
+    <button @click="addTask">Ajouter une tache</button>
+
+    <div :key="index" class="task" v-for="(task, index) in tasks">
+      <h1>{{ task }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodolistComponent",
+  data() {
+    return {
+      newTask: "",
+      tasks: []
+    }
+  },
+  props: {
+    message: {
+      type: String,
+      default: "Liste de tâches"
+    },
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.newTask)
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
